70ce2232f32c94d8f64240728e3657ef1089660f
Hidayatullah <hidaya.2016@sis.smu.edu.sg>
2018-03-28 07:54:34 +0800

    Updated store.js
    Updated store.js to store new users into mysql database

--- store.js
+++ store.js
@@ -1,6 +1,16 @@
+//This file is used to store new users into mysql database
+
+//declaring knex constant to knexfile.js configuration
+//const knex establishes the mysql client destination and establishes connection to the db
+const knex = require('knex')(require('./knexfile'))
+
+//exports the new user from createUser and inserts the username and password into the user table in the db schema
 module.exports = {
-    createUser ({ username, password }) {
-      console.log(`Add user ${username} with password ${password}`)
-      return Promise.resolve()
-    }
+  createUser ({ username, password }) {
+    console.log(`Add user ${username} with password ${password}`)
+    return knex('user').insert({
+      username,
+      password
+    })
+  }
 }
