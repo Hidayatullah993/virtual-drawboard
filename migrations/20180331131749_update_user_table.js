938a2a77658f2ac80ff6e73f92d147efffba0947
Hidayatullah <hidaya.2016@sis.smu.edu.sg>
2018-03-31 14:19:33 +0800

    Update database
    Added random salt and hashing of passwords into the database

---
+++ migrations/20180331131749_update_user_table.js
@@ -0,0 +1,34 @@
+const { saltHashPassword } = require('../store')
+
+exports.up = function up (knex) {
+  return knex.schema
+    .table('user', table => {
+      table.string('salt').notNullable()
+      table.string('encrypted_password').notNullable()
+    })
+    .then(() => knex('user'))
+    .then(users => Promise.all(users.map(convertPassword)))
+    .then(() => {
+      return knex.schema.table('user', table => {
+        table.dropColumn('password')
+      })
+    })
+
+  function convertPassword (user) {
+    const { salt, hash } = saltHashPassword(user.password)
+    return knex('user')
+      .where({ id: user.id })
+      .update({
+        salt,
+        encrypted_password: hash
+      })
+  }
+}
+
+exports.down = function down (knex) {
+  return knex.schema.table('user', t => {
+    t.dropColumn('salt')
+    t.dropColumn('encrypted_password')
+    t.string('password').notNullable()
+  })
+}
--- store.js
+++ store.js
@@ -4,13 +4,38 @@
 //const knex establishes the mysql client destination and establishes connection to the db
 const knex = require('knex')(require('./knexfile'))
 
-//exports the new user from createUser and inserts the username and password into the user table in the db schema
+//requires native node crypto to secure passwords in the database
+const crypto = require('crypto')
+
+//All new users created have a random salt assigned
+//use of crypto to hash passwords
 module.exports = {
+  saltHashPassword,
   createUser ({ username, password }) {
-    console.log(`Add user ${username} with password ${password}`)
+    console.log(`Add user ${username}`)
+    //assigning salt and hash at the same time from the output of saltHashPassword
+    const { salt, hash } = saltHashPassword(password)
     return knex('user').insert({
-      username,
-      password
+      salt,
+      encrypted_password: hash,
+      username
     })
   }
+}
+
+//method returns hashed password with a randomly generated salt
+function saltHashPassword (password) {
+  const salt = randomString()
+  const hash = crypto
+    .createHmac('sha256', salt)
+    .update(password)
+  return {
+    salt,
+    hash: hash.digest('hex')
+  }
+}
+
+//
+function randomString () {
+  return crypto.randomBytes(4).toString('hex')
 }
